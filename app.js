0087be37129f8e3340d02933e30d737705ec82dd
iskandar47 <salimdriai15@gmail.com>
2020-11-15 19:59:35 +0100

    connecting to database

--- app.js
+++ app.js
@@ -9,23 +9,23 @@ app.use(exprees.json());
 
 const URI = "mongodb+srv://salimdriai:Salimdr1892@cluster0.5yvav.mongodb.net/usersdata?retryWrites=true&w=majority";
 
-/* mongoose.connect(URI, {
+mongoose.connect(URI, {
     useNewUrlParser : true,
     useUnifiedTopology : true
 }).then(()=> {
     console.log("successfuly connected to database")
 }).catch((err)=>{
     console.log(err)
-}); */
-mongoose.connect(URI, {
+});
+/* mongoose.connect(URI, {
     useNewUrlParser :true,
     useUnifiedTopology: true
 }, ()=> {
     console.log("successfuly connected to database")
-})
+}) */
 
 const userRouter = require("./routes/user.js");
-app.use("./user", userRouter);
+app.use("/user", userRouter);
 
 app.listen(5000, ()=> {
     console.log("server up and running on port 5000")
--- routes/user.js
+++ routes/user.js
@@ -5,3 +5,42 @@ const passportConfig = require("../passport.js");
 const JWT = require("jsonwebtoken");
 const User = require("../models/user.js");
 const Todo = require("../models/todo.js");
+
+
+const signToken = userID => {
+    return JWT.sign({
+        iss : "CodeWarrior",
+        sub : userID
+    }, "CodeWarrior", {expiresIn : "1h"});
+}
+
+userRouter.post("/register", (req, res) => {
+    const { username, password, role } = req.body;
+    User.findOne( {username}, (err, user) => {
+        if (err)
+            res.status(500).json({message : { msgBody : "Error has occured", msgError : true }});
+        if (user)
+            res.status(400).json({message : { msgBody : "username already taken", msgError : true }});
+        else {
+            const newUser = new User({ username, password, role});
+            newUser.save(err => {
+                if (err)
+                    res.status(500).json({message : { msgBody : "error has occured", msgError : true }})
+                else {
+                    res.status(201).json({message : { msgBody : "account successfuly created", msgError : false }})
+                }
+            });
+        }
+    });
+});
+
+userRouter.post("/login", passport.authenticate('local', {session : false}), (req, res)=>{
+    if (req.isAuthenticated()){
+        const { _id, username, role } = req.user;
+        const token = signToken(_id);
+        res.cookie('access_token', token, {httpOnly : true, sameSite : true });
+        res.status(200).json({isAuthenticated : true, user : {username, role}});
+    }
+})
+
+module.exports = userRouter;
